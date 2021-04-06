bf8ded4203efa29d593a9714a4dab8aabe783b3b
shrushti22 <shrushtivasaniya@gmail.com>
2021-04-06 11:53:17 +0530

    project page cards added

--- src/project.js
+++ src/project.js
@@ -11,6 +11,172 @@ function Project() {
                 <hr />
                 <p className="writer">Martin Luther King Jr.</p>
             </div>
+            <div className="container projects">
+                <div className="row">
+                    <div className="col-4">
+                        <a href="#" className="custom-card">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div className="container project-title">
+                                        <div className="row">
+                                            <div class="col-3">
+                                                <i class="fa fa-github fa-4x icon"></i>
+                                            </div>
+                                            <div class="col-9">
+                                                <h3>Card title</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="project-content">
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <p>
+                                            <span className="tag text-danger is-danger">Tool</span>
+                                            <span className="tag text-primary is-primary">Tool</span>
+                                            <span className="tag text-warning is-warning">Tool</span>
+                                            <span className="tag text-success is-success">Tool</span>
+                                            <span className="tag text-info is-info">Tool</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        
+                    </div>
+                    <div className="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                        <div className="container project-title">
+                            <div className="row">
+                                <div class="col-3">
+                                    <i class="fa fa-github fa-4x icon"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h3>Card title</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="project-content">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>
+                                        <span className="tag text-danger is-danger">Tool</span>
+                                        <span className="tag text-info is-info">Tool</span>
+                                        <span className="tag text-success is-success">Tool</span>
+                                        <span className="tag text-warning is-warning">Tool</span>
+                                        <span className="tag text-primary is-primary">Tool</span>
+                            </p>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                        <div className="container project-title">
+                            <div className="row">
+                                <div class="col-3">
+                                    <i class="fa fa-github fa-4x icon"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h3>Card title</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="project-content">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>
+                                        <span className="tag text-warning is-warning">Tool</span>
+                                        <span className="tag text-success is-success">Tool</span>
+                                        <span className="tag text-danger is-danger">Tool</span>
+                                        <span className="tag text-primary is-primary">Tool</span>
+                                        <span className="tag text-info is-info">Tool</span>
+                            </p>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                        <div className="container project-title">
+                            <div className="row">
+                                <div class="col-3">
+                                    <i class="fa fa-github fa-4x icon"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h3>Card title</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="project-content">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>
+                                        <span className="tag text-warning is-warning">Tool</span>
+                                        <span className="tag text-primary is-primary">Tool</span>
+                                        <span className="tag text-info is-info">Tool</span>
+                                        <span className="tag text-danger is-danger">Tool</span>
+                                        <span className="tag text-success is-success">Tool</span>
+                            </p>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                        <div className="container project-title">
+                            <div className="row">
+                                <div class="col-3">
+                                    <i class="fa fa-github fa-4x icon"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h3>Card title</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="project-content">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>
+                                        <span className="tag text-success is-success">Tool</span>
+                                        <span className="tag text-primary is-primary">Tool</span>
+                                        <span className="tag text-info is-info">Tool</span>
+                                        <span className="tag text-warning is-warning">Tool</span>
+                                        <span className="tag text-danger is-danger">Tool</span>
+                            </p>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                        <div className="container project-title">
+                            <div className="row">
+                                <div class="col-3">
+                                    <i class="fa fa-github fa-4x icon"></i>
+                                </div>
+                                <div class="col-9">
+                                    <h3>Card title</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="project-content">
+                            <p class="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>
+                                        <span className="tag text-success is-success">Tool</span>
+                                        <span className="tag text-danger is-danger">Tool</span>
+                                        <span className="tag text-info is-info">Tool</span>
+                                        <span className="tag text-primary is-primary">Tool</span>
+                                        <span className="tag text-warning is-warning">Tool</span>
+                            </p>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
     
